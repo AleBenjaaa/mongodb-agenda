e7e97c14853be5bcdbc695d0279b2c55fb501a17
AleBenjaaa <alebenj4min@gmail.com>
2024-07-12 19:04:37 -0400

    conexion a coleccion

--- agenda.py
+++ agenda.py
@@ -3,8 +3,16 @@ import pymongo
 try:
     client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-    database = client["agenda"]
+    db = client["agenda"]
     print("conectado a mongo")
 
 except Exception as e:
     print("error mongo")
+
+
+try:
+    collection = db["contactos"]
+    print("conecado a contactos")
+
+except Exception as e:
+    print("error")
