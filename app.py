0079a53ce238a7b17ab9709f5e19b2fe6249bd39
JotaDevMan <jpea195@gmail.com>
2024-07-13 01:37:47 -0400

    Arreglado el error 404 anterior

--- app.py
+++ app.py
@@ -10,22 +10,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
-
-
-@app.route('/Contacto', methods=['POST'])
+@app.route('/contacto', methods=['POST'])
 def nuevo_contacto():
-    contacto = db['contactos']
+    contacto_collection = db.contactos  # Asegúrate de acceder al atributo correcto
     nombre = request.form['nombre']
-    edad = request.fomr['edad']
+    edad = request.form['edad']  # Corregido el error tipográfico aquí
     categoria = request.form['categoria']
     telefono = request.form['telefono']
     direccion = request.form['direccion']
-    favorito = request.form['favorito']
+    favorito = request.form.get('favorito', 'off') == 'on'  # Usar get() con valor predeterminado
 
-    if nombre and edad and categoria and telefono and direccion and favorito:
-        contacto = Contacto(nombre,edad,categoria,telefono,direccion,favorito)
-        contacto.insert_one(contacto.to_dict())
+    if nombre and edad and categoria and telefono and direccion:
+        contacto = Contacto(nombre, edad, categoria, telefono, direccion, favorito)
+        contacto_collection.insert_one(contacto.to_dict())  # Cambiado a contacto_collection
         response = jsonify({
             'nombre': nombre,
             'edad': edad,
@@ -37,20 +34,16 @@ def nuevo_contacto():
         return redirect(url_for('home'))
     else:
         return notFound()
-    
-
 
 @app.errorhandler(404)
 def notFound(error=None):
-    message ={
-        'message': 'No encontrado '+ request.url,
+    message = {
+        'message': 'No encontrado ' + request.url,
         'status': '404 Not Found'
     }
     response = jsonify(message)
     response.status_code = 404
     return response
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True,port=1200)
+    app.run(debug=True, port=1200)
